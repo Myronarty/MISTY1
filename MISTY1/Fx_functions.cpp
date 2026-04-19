8ced896395d9e549c38d13fc3faf097ee43d3d7b
Myronarty <ahamalianmyron@gmail.com>
2026-04-19 18:06:06 +0300

    def sizes of data and keys

--- MISTY1/Fx_functions.cpp
+++ MISTY1/Fx_functions.cpp
@@ -1,2 +1,33 @@
 #include "Fx_functions.h"
 
+uint16_t FI(uint16_t X, uint16_t KI)
+{
+	uint16_t L_0 = X >> 7;
+	uint8_t R_0 = X & 0x7F;
+
+
+
+	uint16_t Y;
+	return Y;
+}
+
+uint32_t FO(uint32_t X, uint64_t KO, uint64_t KI)
+{
+
+	uint32_t Y;
+	return Y;
+}
+
+uint32_t FL(uint32_t X, uint32_t KL)
+{
+
+	uint32_t Y;
+	return Y;
+}
+
+uint32_t FL_1(uint32_t Y, uint32_t KL)
+{
+
+	uint32_t X;
+	return X;
+}
--- MISTY1/Fx_functions.h
+++ MISTY1/Fx_functions.h
@@ -1,2 +1,10 @@
 #pragma once
+#include "S_boxes.h"
 
+uint16_t FI(uint16_t X, uint16_t KI);
+
+uint32_t FO(uint32_t X, uint64_t KO, uint64_t KI);
+
+uint32_t FL(uint32_t X, uint32_t KL);
+
+uint32_t FL_1(uint32_t Y, uint32_t KL);
