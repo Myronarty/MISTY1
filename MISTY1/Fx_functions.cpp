fe3e2da2dbf0cd9d18e722d40167a40468b4d5cb
Myronarty <ahamalianmyron@gmail.com>
2026-04-19 18:57:31 +0300

    FI done as in doc

--- MISTY1/Fx_functions.cpp
+++ MISTY1/Fx_functions.cpp
@@ -5,9 +5,19 @@ uint16_t FI(uint16_t X, uint16_t KI)
 	uint16_t L_0 = X >> 7;
 	uint8_t R_0 = X & 0x7F;
 
+	uint8_t KI_L = KI & 0x7F;
+	uint16_t KI_R = X >> 7;
 
+	uint16_t R_1 = S_9[L_0] ^ Extnd(R_0);
+	uint8_t L_1 = R_0;
 
-	uint16_t Y;
+	uint8_t R_2 = S_7[L_1] ^ Trunc(R_1) ^ KI_L;
+	uint16_t L_2 = R_1 ^ KI_R;
+
+	uint16_t R_3 = S_9[L_2] ^ Extnd(R_2);
+	uint8_t L_3 = R_2;
+
+	uint16_t Y = (uint16_t(L_3) << 9) | R_3;
 	return Y;
 }
 
