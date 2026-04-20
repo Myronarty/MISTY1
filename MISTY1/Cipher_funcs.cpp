4ef5392c56b37744b00070c2e84b906654356c65
Myronarty <ahamalianmyron@gmail.com>
2026-04-20 21:45:23 +0300

    names for EncryptBlock/DecryptBlock

--- MISTY1/Cipher_funcs.cpp
+++ MISTY1/Cipher_funcs.cpp
@@ -1,6 +1,6 @@
 #include "Cipher_funcs.h"
 
-uint64_t MISTY1(uint64_t P, uint16_t(&KL)[10][2], uint16_t(&KO)[8][4], uint16_t(&KI)[8][3])
+uint64_t EncryptBlock(uint64_t P, uint16_t(&KL)[10][2], uint16_t(&KO)[8][4], uint16_t(&KI)[8][3])
 {
 	uint32_t L = uint32_t(P >> 32);
 	uint32_t R = uint32_t(P);
@@ -30,7 +30,7 @@ uint64_t MISTY1(uint64_t P, uint16_t(&KL)[10][2], uint16_t(&KO)[8][4], uint16_t(
 	return C;
 }
 
-uint64_t deMISTY1(uint64_t C, uint16_t(&KL)[10][2], uint16_t(&KO)[8][4], uint16_t(&KI)[8][3])
+uint64_t DecryptBlock(uint64_t C, uint16_t(&KL)[10][2], uint16_t(&KO)[8][4], uint16_t(&KI)[8][3])
 {
 	uint32_t L = uint32_t(C >> 32);
 	uint32_t R = uint32_t(C);
--- MISTY1/Cipher_funcs.h
+++ MISTY1/Cipher_funcs.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Key.h"
 
-uint64_t MISTY1(uint64_t P, uint16_t(&KL)[10][2], uint16_t(&KO)[8][4], uint16_t(&KI)[8][3]);
+uint64_t EncryptBlock(uint64_t P, uint16_t(&KL)[10][2], uint16_t(&KO)[8][4], uint16_t(&KI)[8][3]);
 
-uint64_t deMISTY1(uint64_t C, uint16_t(&KL)[10][2], uint16_t(&KO)[8][4], uint16_t(&KI)[8][3]);
+uint64_t DecryptBlock(uint64_t C, uint16_t(&KL)[10][2], uint16_t(&KO)[8][4], uint16_t(&KI)[8][3]);
+
+
