acb0635ba817892d415f75e9cf3516f71db29e63
Myronarty <ahamalianmyron@gmail.com>
2026-04-20 16:55:40 +0300

    Expansion of main Key done

--- MISTY1/Key.cpp
+++ MISTY1/Key.cpp
@@ -31,11 +31,39 @@ void getK(string file_name, uint16_t(&K)[8])
     file.close();
 }
 
-void Expansion(uint64_t K[2], RoundKeys(&Keys)[10])
+void Expansion(uint16_t K[8], uint16_t(&KL)[8][2], uint16_t(&KO)[8][4], uint16_t(&KI)[10][3])
 {
-    uint64_t K_[2] = { 0 };
-    uint64_t u1 = 0;
-    uint64_t u2 = 0;
+    uint16_t K_[8] = { 0 };
 
-    for(int i = 0; i < )
+    for (int i = 0; i < 7; i++)
+    {
+        K_[i] = FI(K[i], K[i + 1]);
+    }
+    K_[7] = FI(K[7], K[0]);
+
+    for (int i = 0; i < 8; i++)
+    {
+        KO[i][0] = K[i % 8];
+        KO[i][1] = K[(i + 2) % 8];
+        KO[i][2] = K[(i + 7) % 8];
+        KO[i][3] = K[(i + 4) % 8];
+
+        KI[i][0] = K_[(i + 5) % 8];
+        KI[i][1] = K_[(i + 1) % 8];
+        KI[i][2] = K_[(i + 3) % 8];
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (i % 2 == 0)
+        {
+            KL[i][0] = K[(i / 2) % 8];
+            KL[i][1] = K_[(i / 2 + 6) % 8];
+        }
+        else
+        {
+            KL[i][0] = K_[((i + 1) / 2 + 1) % 8];
+            KL[i][1] = K[((i + 1) / 2 + 3) % 8];
+        }
+    }
 }
--- MISTY1/Key.h
+++ MISTY1/Key.h
@@ -9,13 +9,6 @@
 
 using namespace std;
 
-struct RoundKeys
-{
-    uint16_t KL[2];
-    uint16_t KO[4];
-    uint16_t KI[3];
-};
-
 void getK(string file_name, uint16_t(&K)[8]);
 
-void Expansion(uint64_t K[2], RoundKeys(&Keys)[10]);
+void Expansion(uint16_t K[8], uint16_t(&KL)[8][2], uint16_t(&KO)[8][4], uint16_t(&KI)[10][3]);
