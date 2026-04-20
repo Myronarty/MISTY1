0ba79ea4c345d4cf375f2599f0adc9b42f934d29
Myronarty <ahamalianmyron@gmail.com>
2026-04-20 21:36:10 +0300

    deMISTY1 is done from MISTY1

--- MISTY1/Cipher_funcs.cpp
+++ MISTY1/Cipher_funcs.cpp
@@ -32,5 +32,30 @@ uint64_t MISTY1(uint64_t P, uint16_t(&KL)[10][2], uint16_t(&KO)[8][4], uint16_t(
 
 uint64_t deMISTY1(uint64_t C, uint16_t(&KL)[10][2], uint16_t(&KO)[8][4], uint16_t(&KI)[8][3])
 {
+	uint32_t L = uint32_t(C >> 32);
+	uint32_t R = uint32_t(C);
 
+	uint32_t L_prev = L;
+	uint32_t R_prev = R;
+
+	uint32_t L_curr = 0, R_curr = 0, L_next = 0, R_next = 0;
+
+	for (int i = 9; i > 1; i -= 2)
+	{
+		R_curr = FL_1(L_prev, KL[i]);
+		L_curr = FL_1(R_prev, KL[i - 1]) ^ FO(R_curr, KO[i - 2], KI[i - 2]);
+		
+		L_next = R_curr ^ FO(L_curr, KO[i - 3], KI[i - 3]);
+		R_next = L_curr;
+
+		L_prev = L_next;
+		R_prev = R_next;
+	}
+
+	uint32_t R_final = FL_1(L_prev, KL[1]);
+	uint32_t L_final = FL_1(R_prev, KL[0]);
+
+	uint64_t P = ((uint64_t)L_final << 32) | (uint64_t)R_final;
+
+	return P;
 }
