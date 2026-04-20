696714a88b17834993e17e95ba41c508578c2118
Myronarty <ahamalianmyron@gmail.com>
2026-04-20 22:41:58 +0300

    dubl 2

--- MISTY1/main.cpp
+++ MISTY1/main.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-	string file = "D:/problems/term_6/crypto/pu_pu_pu.txt";
-	string out = "D:/problems/term_6/crypto/pu_pu_pu.txt.m";
-	string file_ = "D:/problems/term_6/crypto/pu_pu_pu.txt.m";
-	string out_ = "D:/problems/term_6/crypto/pu_pu_pu__.txt";
-	string k = "D:/problems/term_6/crypto/k.txt";
+	string file = "D:/problems/term_6/crypto/test/pu_pu_pu.txt";
+	string out = "D:/problems/term_6/crypto/test/pu_pu_pu.txt.m";
+	string file_ = "D:/problems/term_6/crypto/test/pu_pu_pu.txt.m";
+	string out_ = "D:/problems/term_6/crypto/test/pu_pu_pu__.txt";
+	string k = "D:/problems/term_6/crypto/test/k.txt";
 
 	MISTY1(file, out, k);
 	deMISTY1(file_, out, k);
